5f17a6d6041efcb21d000718df462aeed17cab30
klmork <klmork@calpoly.edu>
2023-10-17 16:46:04 -0700

    drawButterfly: Add Function
    Add drawButterfly function from lab 1.

--- src/sketch.js
+++ src/sketch.js
@@ -9,4 +9,71 @@ function setup() {
 
 function draw() {
   background(220);
+  drawButterfly(200, 200, 0.3);
+}
+
+function drawButterfly(x, y, s) {
+  // butterfly
+  push();
+  translate(x, y);
+  scale(s);
+
+  // --------- BACK WING ----------
+  fill(9, 40, 153);
+  beginShape();
+  curveVertex(-300, -90); // guide
+  curveVertex(-250, -100); // left top bump
+  curveVertex(-175, -95); // valley
+  curveVertex(-90, -125); // top right
+  //curveVertex(-50, -55);
+  curveVertex(0, 0); // origin
+  curveVertex(-100, 3); // bottom
+  curveVertex(-250, -100); // left again
+  curveVertex(-255, -200); // guide
+  endShape();
+
+  fill(34, 150, 230, 100); // purple
+  ellipse(-100, -50, 80, 80);
+  ellipse(-100, -50, 100, 100);
+
+  // --------- FRONT WING ----------
+  fill(34, 150, 230); // light blue
+  stroke(14, 96, 153); // darker blue green
+  //strokeWeight(10);
+  beginShape();
+  curveVertex(-75, -145); // guide
+  curveVertex(0, -140); // careful which start point
+  curveVertex(75, -145); // top right bump
+  curveVertex(0, 0); // bottom point
+  curveVertex(-75, -145); // left top bump
+  curveVertex(0, -140); // valley
+  curveVertex(75, -145); // guide
+  endShape();
+
+  fill(107, 0, 230, 100); // purple
+  ellipse(0, -30, 30, 55);
+  ellipse(0, -40, 40, 75);
+
+  // --------- HEAD ----------
+  push();
+  translate(100, -35);
+  rotate(PI / 3);
+  fill(107, 0, 230, 100); // purple
+  noStroke();
+  // head
+  ellipse(0, 0, 20, 40);
+  ellipse(0, 0, 30, 50);
+  stroke(107, 0, 230, 100);
+  strokeWeight(10);
+  line(0, 60, 0, 210);
+  noStroke();
+  // chest
+  translate(0, 60);
+  ellipse(0, 0, 20, 50);
+  // back
+  translate(0, 150);
+  ellipse(0, 0, 20, 40);
+  pop();
+
+  pop();
 }
